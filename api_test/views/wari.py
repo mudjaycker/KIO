25e61a6e67e2f3c44e4a00fa17e851365e767c83
mudjaycker <tombutoyi@gmail.com>
2022-08-06 20:38:11 +0200

    3rd part wari view

---
+++ api_test/views/wari.py
@@ -0,0 +1,38 @@
+from models.wari import Wari
+from models.dependancies import User
+from schemas.wari import WariBody, WariQuery, WariBodyDeserializer
+from .dependancies import *
+
+
+tag = Tag(name="api_test", description="Wari account")
+
+api = APIBlueprint(
+    "/api_test/wari",
+    __name__,
+    url_prefix="/api",
+    abp_tags=[tag],
+    # disable openapi UI
+    doc_ui=True,
+)
+
+
+@api.get("/list/<int:user>")
+def get_api_test(path: WariQuery):
+    """Get all Trasanction for a user
+    """
+    return {
+        "datas": [
+            dict(WariBodyDeserializer(**transaction.to_json()))
+            for transaction in Wari.query.filter_by(user=path.user)
+        ]
+    }
+
+
+@api.post("/create")
+def create_api_test(body: WariBody):
+    "Post a api_test"
+    wari = Wari(**body.dict())
+    wari.save_to_db()
+    print(Wari.query.all())
+    return {"code": 0, "message": "ok", "datas": body.dict()}
+
