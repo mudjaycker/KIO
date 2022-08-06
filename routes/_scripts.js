e2f515d5cdeff2ceaca42ad65cdf45c4acb9ca61
mudjaycker <tombutoyi@gmail.com>
2022-08-06 20:38:53 +0200

    useful script

---
+++ routes/_scripts.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+module.exports = {
+  subword: function (text, start, end = text.length - 1) {
+    let text_res = "";
+    for (let i = start; i <= end; i++) {
+      text_res += text[i];
+    }
+    return text_res;
+  },
+  excludeChar: function (text, char) {
+    let s = "";
+    for (i of text) {
+      if (i !== char) {
+        s += i;
+      } else if (char == "*") {
+        break;
+      }
+    }
+    return s;
+  },
+
+  checkIfUserExist: async function (url) {
+    let bool;
+    let resp;
+    await axios
+      .get(url)
+      .then((res) => {
+        bool = true;
+        resp = res;
+      })
+      .catch((err) => {
+        bool = false;
+        resp = err;
+      });
+    return [bool, resp];
+  },
+
+  getKioUser: async function (Instance, phoneNumber) {
+    user = await Instance.find({ phoneNumber: phoneNumber });
+    return user;
+  },
+};
