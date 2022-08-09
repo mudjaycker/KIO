9f772154b039c70e44ebe139b4b5980f7d0938b2
mudjaycker <tombutoyi@gmail.com>
2022-08-09 11:56:11 +0200

    structure the code

--- routes/kio.js
+++ routes/kio.js
@@ -1,118 +1,45 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/kio");
+const KioObject = require("./kioObject");
+const fs = require("fs");
 
-const thirdPartsUrl = "http://127.0.0.1:5000/api";
-
-const {
-  subword,
-  checkIfUserExist,
-  getKioUser,
-  excludeChar,
-} = require("./_scripts");
-
-class KioRoute {
-  constructor() {
-    this.thirdPartsUrl = "http://127.0.0.1:5000/api";
-
-    this.request;
-    this.results;
-    this.response;
-    this.password;
-    this.text;
-
-    this.phoneNumber;
-    this.serviceCode;
-    this.sessionId;
-    this.networkCode;
-
-    // map to manage ussd cases
-    this.ussdCases = new Map();
-    this.ussdCases.set("", () => this.askForPassword());
-  }
-
-  sendMenu1() {
-    this.response = `CON 1- Get my balance account
-    2- Services
-    3- create an account if not exists`;
-  }
-
-  async whenAuthentified() {
-    const users = await User.find();
-    let userExists;
-    let p;
-    users.forEach((user) => {
-      if (this.text.includes(user.password) && user.phoneNumber == this.phoneNumber) {
-        this.password = user.password;
-        p = this.password
-
-        userExists = true;
-        // proctected action need auth to be done
-        this.ussdCases.set(this.password + "*1", () => this.balanceSolde());
-        this.ussdCases.set(this.password + "*1*0", () => this.sendMenu1());
-        this.ussdCases.set(this.password + "*2", () => this.sendMenu2());
-        this.ussdCases.set(this.password + "*1*0", () => this.sendMenu1());
-        this.ussdCases.set(this.password + "*1*" + this.password, () => this.sendMenu1());
-        this.ussdCases.set(this.password + "*2*0", () => this.sendMenu2());
-        this.ussdCases.set(this.password + "*2*1",  () => this.wari());
-      } else {
-        userExists = false;
-        p = "unknown password";
-      }
-    });
-    return [userExists, p];
-  }
-  async balanceSolde(){
-      const user = await User.findOne({ phoneNumber: this.phoneNumber, password: this.password})
-      this.response = `Your balance is ${user.balance} fbu`
-  }
-  
-  askForPassword() {
-    this.response = `CON first enter your password`;
-  }
-
-  sendMenu2() {
-    this.response = `CON welcome to services make your choice
-    1- WAARI
-    2- World remit
-    3- Canal+
-    3- Buy vochers
-    5- Moto assurance`;
-  }
-
-  async wari() {
-    let thirdPartUserExists = await checkIfUserExist(
-      this.thirdPartsUrl + "/user/" + this.phoneNumber
-    );
-    console.log("in wari");
-    if (thirdPartUserExists[0]) {
-      this.response = `CON 1 - send money`;
-    } else {
-      this.response = `This wari users does not exist`;
-    }
-  }
-}
 
 router.post("/kio/wari", async (req, res) => {
-  let route = new KioRoute();
+  let route = new KioObject();
   let { phoneNumber, serviceCode, text, sessionId, networkCode } = req.body;
-  route.phoneNumber = phoneNumber;
-  route.serviceCode = serviceCode;
-  route.sessionId = sessionId;
-  route.networkCode = networkCode;
+  [
+    route.phoneNumber,
+    route.serviceCode,
+    route.text,
+    route.sessionId,
+    route.networkCode,
+  ] = [phoneNumber, serviceCode, text, sessionId, networkCode];
 
   route.text = text;
+  let userTempo = JSON.parse(fs.readFileSync("userTempo.json"));
+  console.log(
+    route.text.includes(userTempo.password + "*" + userTempo.password)
+  );
+
   if (text == "") {
-  await route.ussdCases.get("")();
+    await route.ussdCases.get("")();
   }
-  if ((await route.whenAuthentified()) != [] && route.text.length >= 4) {
-    if (route.text == route.password) {
-      route.sendMenu1()
-    } else {
-     await route.ussdCases.get(route.text)();
+ else if (route.text != "") {
+    if (
+      (await route.whenAuthentified()) &&
+      route.text.length >= 4 &&
+      route.text.length < 9
+    ) {
+      if (route.text == route.password) {
+        route.sendMenu1();
+      } else {
+        await route.ussdCases.get(route.text)();
+      }
     }
-  }
- 
+    else {
+      route.response = "END bad code"
+    }
+  } 
   res.send(route.response);
 });
 module.exports = router;
---
+++ routes/kioObject.js
@@ -0,0 +1,85 @@
+const User = require("../models/kio");
+const thirdPartsUrl = "http://127.0.0.1:5000/api";
+const { checkIfUserExist } = require("./_scripts");
+
+class KioObject {
+  constructor() {
+    this.thirdPartsUrl = "http://127.0.0.1:5000/api";
+
+    this.request;
+    this.results;
+    this.response;
+    this.password;
+    this.text;
+    this.textPassword;
+
+    this.phoneNumber;
+    this.serviceCode;
+    this.sessionId;
+    this.networkCode;
+
+    // map to manage ussd cases
+    this.ussdCases = new Map();
+    this.ussdCases.set("", () => this.askForPassword());
+  }
+
+
+  sendMenu1() {
+    this.response = `CON 1- Get my balance account
+    2- Services
+    3- create an account if not exists`;
+  }
+
+  sendMenu2() {
+    this.response = `CON welcome to services make your choice
+    1- WAARI
+    2- World remit
+    3- Canal+
+    3- Buy vochers
+    5- Moto assurance`;
+  }
+
+  async whenAuthentified() {
+    const users = await User.find();
+    let userExists;
+    users.forEach((user) => {
+      if (
+        this.text.includes(user.password) &&
+        user.phoneNumber == this.phoneNumber
+      ) {
+        this.password = user.password;
+
+        userExists = true;
+        // proctected action need auth to be done
+        this.ussdCases.set(this.password + "*1", () => this.balanceSolde());
+        this.ussdCases.set(this.password + "*2", () => this.sendMenu2());
+        this.ussdCases.set(this.password + "*1*0", () => this.sendMenu1());
+        this.ussdCases.set(this.password + "*1*0", () => this.sendMenu1());
+        this.ussdCases.set(this.password + "*1*" + this.password, () => this.sendMenu1());
+        this.ussdCases.set(this.password + "*2*0", () => this.sendMenu2());
+        this.ussdCases.set(this.password + "*2*1", () => this.wari());
+      } else {
+        userExists = false;
+      }
+    });
+    return userExists;
+  }
+  async balanceSolde() {
+    const user = await User.findOne({
+      phoneNumber: this.phoneNumber,
+      password: this.password,
+    });
+    this.response = `Your balance is ${user.balance} fbu`;
+  }
+
+  async wari() {
+    let thirdPartUserExists = await checkIfUserExist( thirdPartsUrl + "/user/" + this.phoneNumber );
+    if (thirdPartUserExists[0]) {
+      this.response = `CON 1 - send money`;
+    } else {
+      this.response = `This wari users does not exist`;
+    }
+  }
+}
+
+module.exports = KioObject
