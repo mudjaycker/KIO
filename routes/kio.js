f4752df1842a751419f7af32aec9f6b11159f01b
mudjaycker <tombutoyi@gmail.com>
2022-08-10 06:18:47 +0200

    route to kio services

--- routes/kio.js
+++ routes/kio.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const KioObject = require("./kioObject");
+const KioUserService = require("./kioUserServices")
 const fs = require("fs");
 
 
-router.post("/kio/wari", async (req, res) => {
+router.post("/kio", async (req, res) => {
   let route = new KioObject();
   let { phoneNumber, serviceCode, text, sessionId, networkCode } = req.body;
   [
@@ -16,19 +17,12 @@ router.post("/kio/wari", async (req, res) => {
   ] = [phoneNumber, serviceCode, text, sessionId, networkCode];
 
   route.text = text;
-  let userTempo = JSON.parse(fs.readFileSync("userTempo.json"));
-  console.log(
-    route.text.includes(userTempo.password + "*" + userTempo.password)
-  );
-
   if (text == "") {
     await route.ussdCases.get("")();
   }
  else if (route.text != "") {
     if (
-      (await route.whenAuthentified()) &&
-      route.text.length >= 4 &&
-      route.text.length < 9
+      (await route.whenAuthentified()) 
     ) {
       if (route.text == route.password) {
         route.sendMenu1();
@@ -36,10 +30,21 @@ router.post("/kio/wari", async (req, res) => {
         await route.ussdCases.get(route.text)();
       }
     }
+  else if(route.text == "3"){
+    res.redirect(200, "kio/user-services")
+  }
     else {
       route.response = "END bad code"
     }
   } 
   res.send(route.response);
 });
+
+router.post("/kio/user-services", async(req, res)=>{
+  let routeUser = new KioUserService()
+  let { phoneNumber, serviceCode, text, sessionId, networkCode } = req.body;
+  [routeUser.phoneNumber, routeUser.text] = [phoneNumber, text]
+   routeUser.run()
+  res.send(routeUser.response)
+})
 module.exports = router;
